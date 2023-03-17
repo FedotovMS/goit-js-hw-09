c5e19345967f13a08ff572e91fd09b67daea8afa
Mykola Fedotov <fedotovns@gmail.com>
2023-03-17 17:05:42 +0100

    fixed 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,17 +20,21 @@ const amountInput = form.elements.amount;
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
-  const delay = Number(delayInput.value);
+  let currentDelay = Number(delayInput.value);
   const step = Number(stepInput.value);
   const amount = Number(amountInput.value);
 
   for (let position = 1; position <= amount; position++) {
     try {
-      await createPromise(position, delay);
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      await createPromise(position, currentDelay);
+      Notiflix.Notify.success(
+        `✅ Fulfilled promise ${position} in ${currentDelay}ms`
+      );
     } catch (error) {
-      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(
+        `❌ Rejected promise ${position} in ${currentDelay}ms`
+      );
     }
-    delay += step;
+    currentDelay += step;
   }
 });
